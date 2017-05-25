0463c22af2cf86738e26eb63cc29ea1f23c94cd2
Mateusz Majcher <mmajche@student.agh.edu.pl>
2017-05-25 23:51:44 +0200

    Basic Database

---
+++ microservices/database/src/main/java/database/Crodis.java
@@ -0,0 +1,78 @@
+package database;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Crodis implements Serializable {
+    public static class Item implements Serializable{
+        float latitude;
+        float longitude;
+        float radius;
+        Map<String, Float> conditions;
+
+        // Temperature [Celsius]
+        public static String CONDITION_TEMPERATURE = "temperature";
+
+        public float getLatitude() {
+            return latitude;
+        }
+
+        public void setLatitude(float latitude) {
+            this.latitude = latitude;
+        }
+
+        public float getLongitude() {
+            return longitude;
+        }
+
+        public void setLongitude(float longitude) {
+            this.longitude = longitude;
+        }
+
+        public float getRadius() {
+            return radius;
+        }
+
+        public void setRadius(float radius) {
+            this.radius = radius;
+        }
+
+        public Map<String, Float> getConditions() {
+            return conditions;
+        }
+
+        public void setConditions(Map<String, Float> conditions) {
+            this.conditions = conditions;
+        }
+    }
+
+    private String source;
+    private List<Item> items;
+
+    public Crodis(){
+        this.items = new ArrayList<Item>();
+    }
+
+    public Crodis(String source) {
+        this.source = source;
+        this.items = new ArrayList<Item>();
+    }
+
+    public void addItem(float lat, float lon, float radius, Map<String, Float> conditions) {
+        Item item = new Item();
+        item.setLatitude(lat);
+        item.setLongitude(lon);
+        item.setRadius(radius);
+        Map<String, Float> clonedConditions = new HashMap<String, Float>();
+        clonedConditions.putAll(conditions);
+        item.setConditions(clonedConditions);
+        this.items.add(item);
+    }
+
+    public String getSource() {
+        return this.source;
+    }
+    public List<Item> getItems() { return this.items; }
+}
