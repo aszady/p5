5d0488ee283603bb86576cd8c59a15c9b7272cf7
Adam Szady <git@adsz.pl>
2017-05-30 12:16:47 +0200

    ICM: return multiple Crodis items for lat+lon+radius under /area

--- microservices/translators/icm/service.py
+++ microservices/translators/icm/service.py
@@ -1,27 +1,31 @@
 import json
 from datetime import datetime, timedelta
 
+import icmparser.coords
 import icmparser.current
 
 from tornado.ioloop import IOLoop
 import tornado.web
 
-def mgram_to_crodis(mgram, at=None):
+
+def mgram_to_crodis_item(mgram, at):
+    return {
+        'lat': mgram.lat,
+        'lon': mgram.lon,
+        'radius': 5,
+        'conditions': {
+            'temperature': mgram.get_temperature(at),
+            'temperature_delta': (mgram.get_temperature(at + timedelta(hours=3)) - mgram.get_temperature(at)) / 3
+        }
+    }
+
+def mgrams_to_crodis(mgrams, at=None):
     if at is None:
         at = datetime.utcnow()
+    mgrams = list(mgrams)
     return {
-        'source': 'icm:'+mgram.fdate,
-        'items': [
-            {
-                'lat': mgram.lat,
-                'lon': mgram.lon,
-                'radius': 5,
-                'conditions': {
-                    'temperature': mgram.get_temperature(at),
-                    'temperature_delta': (mgram.get_temperature(at + timedelta(hours=3)) - mgram.get_temperature(at)) / 3
-                }
-            }
-        ]
+        'source': 'icm:'+mgrams[0].fdate,
+        'items': list(map(lambda mgram: mgram_to_crodis_item(mgram, at), mgrams))
     }
 
 
@@ -35,15 +39,17 @@ class PointHandler(RestHandler):
         lat = float(self.get_query_argument('latitude'))
         lon = float(self.get_query_argument('longitude'))
         mgram = icmparser.current.CurrentMeteogram(lat, lon)
-        self.write(json.dumps(mgram_to_crodis(mgram)))
+        self.write(json.dumps(mgrams_to_crodis([mgram])))
 
 
 class AreaHandler(RestHandler):
     def get(self):
         lat = float(self.get_query_argument('latitude'))
         lon = float(self.get_query_argument('longitude'))
-        mgram = icmparser.current.CurrentMeteogram(lat, lon)
-        self.write(json.dumps(mgram_to_crodis(mgram)))
+        radius = float(self.get_query_argument('radius'))
+        grid = list(icmparser.coords.latlonradius2grid(lat, lon, radius))
+        mgrams = map(lambda latlon: icmparser.current.CurrentMeteogram(*latlon), grid)
+        self.write(json.dumps(mgrams_to_crodis(mgrams)))
 
 
 class Application(tornado.web.Application):
