d09bf295b908f5bfc8fddb44d8c88213eb51ab3c
Sambard <kpodsia@student.agh.edu.pl>
2017-06-13 18:04:34 +0200

    Eureka usage fix

--- microservices/updater/src/main/java/org/p5/updater/controllers/ApiController.java
+++ microservices/updater/src/main/java/org/p5/updater/controllers/ApiController.java
@@ -3,11 +3,11 @@ package org.p5.updater.controllers;
 import org.p5.commons.crodis.Crodis;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.URI;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,17 +17,19 @@ public class ApiController {
 
     private final DiscoveryClient discoveryClient;
 
+    @Value("${p5.crodis.radius}")
+    private double radius;
+
     @Value("${p5.yacs}")
     private String yacsName;
 
     @Value("${p5.translators}")
-    private List<String> translatorUrls;
+    private List<String> translatorNames;
 
     @Autowired
     public ApiController(DiscoveryClient discoveryClient) {
         this.discoveryClient = discoveryClient;
     }
-
 
     @RequestMapping(value = "/location", method = RequestMethod.POST)
     public void calculateLocation(@RequestParam double latitude,
@@ -35,14 +37,24 @@ public class ApiController {
         Map<String, Double> coordinates = new HashMap<>(2);
         coordinates.put("latitude", latitude);
         coordinates.put("longitude", longitude);
-        for (String url : translatorUrls) {
+        coordinates.put("radius", radius);
+        for (String name : translatorNames) {
             RestTemplate template = new RestTemplate();
-            saveCrodis(template.getForObject(url + "/location", Crodis.class, coordinates));
+            List<ServiceInstance> serviceInstances = discoveryClient.getInstances(name);
+            if (serviceInstances.size() != 0) {
+                String url = serviceInstances.get(0).getUri() + "/area";
+//                String url = "http://localhost:4412/area";
+                Crodis crodis = template.getForObject(url, Crodis.class, coordinates);
+                saveCrodis(crodis);
+            }
         }
     }
 
     private void saveCrodis(Crodis crodis) {
         RestTemplate template = new RestTemplate();
-        template.put(discoveryClient.getInstances(yacsName).get(0).getUri(), crodis);
+        List<ServiceInstance> serviceInstances = discoveryClient.getInstances(yacsName);
+        if (serviceInstances.size() != 0) {
+            template.put(serviceInstances.get(0).getUri(), crodis);
+        }
     }
 }
